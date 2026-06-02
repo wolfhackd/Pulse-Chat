84b29197c110b0ec22c979f3335478e7b4db54f9
Mauro Leal <wolfhackd@gmail.com>
2026-06-02 09:21:59 -0300

    feat: server index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,30 @@
-import app from './server';
-import config from '../config.json';
+import fastify from "fastify";
+import { Server } from "socket.io";
 
-// Start the application by listening to specific port
-const port = Number(process.env.PORT || config.PORT || 8080);
-app.listen(port, () => {
-  console.info('Express application started on port: ' + port);
-});
 
+
+const app = fastify();
+
+const io = new Server(app.server,{
+    cors:{
+        origin:"*",
+    }
+})
+
+//Posso gerenciar os eventos do socket.io aqui
+
+io.on("connection",(socket)=>{
+    console.log("Usuário conectado", socket.id);
+
+    socket.on("send_message",(message)=>{
+        io.emit("receive_message", {
+            message,
+            id: socket.id,
+        })
+    })
+})
+
+
+app.listen({port:3333},()=>{
+    console.log("Server is running on port localhost:3333");    
+})
