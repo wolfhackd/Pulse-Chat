31edd31c9262614a66f4657f776dfe52f62e1551
Mauro Leal <wolfhackd@gmail.com>
2026-06-04 00:13:28 -0300

    feat: jwt Utils

---
+++ backend/src/shared/utils/jwt.util.ts
@@ -0,0 +1,33 @@
+import jwt, { type JwtPayload, type SignOptions } from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+
+export interface UserPayload extends JwtPayload {
+    username: string;
+    userId: string;
+    email: string;
+}
+
+export const JwtUtil = {
+
+    generateToken: (payload: UserPayload, options?: SignOptions) =>{
+        const defaultOptions: SignOptions = {
+            expiresIn: '1h'
+        };
+        return jwt.sign(payload, JWT_SECRET, { ...defaultOptions, ...options });
+    },
+
+    verifyToken: (token: string): UserPayload =>{
+        try {
+            return jwt.verify(token, JWT_SECRET) as UserPayload;
+        } catch (err) {
+            throw new Error('Invalid token');
+        }
+    },
+
+   decodeToken(token: string): UserPayload | null {
+    return jwt.decode(token) as UserPayload | null;
+  }
+}
+
+
