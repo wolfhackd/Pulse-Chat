86b2afdfd6489ad1cb72f9d8bdccb930c9c15fa1
Mauro Leal <wolfhackd@gmail.com>
2026-07-14 09:31:09 -0300

    feat: typing event

--- backend/src/socket/handlers/user.handler.ts
+++ backend/src/socket/handlers/user.handler.ts
@@ -3,10 +3,19 @@ import type { Server, Socket } from "socket.io";
 
 export function registerUserEvents(io: Server, socket: Socket){
 
+    socket.on("typing:start", (roomId) => {
+        socket.to(roomId).emit("typing:start", socket.data.user.username);
+    })
+
+    socket.on("typing:stop", (roomId) => {
+        socket.to(roomId).emit("typing:stop", {
+            username: socket.data.user.username,
+    })})
+
     socket.on("disconnect", ()=>{
             io.emit('user_left', {
             username: socket.data.user.username,
             message: `Usuário ${socket.data.user.username} saiu da sala`
         });
-    });
-}
+    })
+};
