b0e0d734a973b6b1707d2694b2596410a2babd04
Mauro Leal <wolfhackd@gmail.com>
2026-07-14 08:35:25 -0300

    feat: onlineUsers list event

--- backend/src/socket/handlers/room.handler.ts
+++ backend/src/socket/handlers/room.handler.ts
@@ -1,11 +1,25 @@
 import type { Server, Socket } from "socket.io";
 import { roomService } from "../../modules/rooms/room.routes.js";
 
+//posso fazer outro evento que emite a lista de usuários online
+const getOnlineUsers = async (io: Server, roomId: string) => {
+    const sockets = await io.in(roomId).fetchSockets();
 
+    return sockets.map(socket => ({
+        userId: socket.data.user.userId,
+        username: socket.data.user.username,
+    }));
+};
+
+const updateOnlineUsers = async (io: Server, roomId: string) => {
+    const users = await getOnlineUsers(io, roomId);
+
+    io.to(roomId).emit("room:onlineUsers", users);
+};
 
 export function registerRoomEvents(io: Server, socket: Socket){
 
-    console.log("Registrando eventos da sala");
+    // console.log("Registrando eventos da sala");
 
     socket.on('join_room', async (roomId) => {
         try{
@@ -16,23 +30,22 @@ export function registerRoomEvents(io: Server, socket: Socket){
 
             socket.join(roomId);
 
-            io.to(roomId).emit('user_joined', {
-                username: socket.data.user.username,
-                message: `Usuário ${socket.data.user.username} entrou na sala ${room.roomName}`
-            });
-            //tenho que chamar um update de usuários online na sala
+            await updateOnlineUsers(io, roomId);
+
         } catch (err) {
             console.error(err);
             socket.emit("error", "Erro ao entrar na sala");
         }
     })
 
-    socket.on("leave_room", (roomId) => {
-        socket.leave(roomId);
-        console.log(`Usuário ${socket.id} saiu da sala`);
-        io.to(roomId).emit('user_left', {
-            username: socket.data.user.username,
-            message: `Usuário ${socket.data.user.username} saiu da sala`
-        });
+   socket.on("leave_room", async (roomId) => {
+    socket.leave(roomId);
+
+    io.to(roomId).emit("user_left", {
+        username: socket.data.user.username,
+        message: `Usuário ${socket.data.user.username} saiu da sala`,
     });
+
+    await updateOnlineUsers(io, roomId);
+});
 };
